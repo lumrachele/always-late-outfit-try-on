6e9b5f6c34ade426008db97b74494f61495c5628
lumrachele <rlum894@gmail.com>
2024-08-15 20:50:58 -0400

    replace tops

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,6 +1,13 @@
-import hoodie from "../img/hoodie.png";
-import dress from "../img/dress.png";
-import tee from "../img/tee.png";
+import dragonHoodie from "../img/dragon-hoodie.png";
+import goodBadBabyTee from "../img/good-bad-baby-tee.png";
+import tribalTank from "../img/tribal-tank.png";
+import xboxPP from "../img/xbox-pp.png";
 
 // not sure why these aren't strings but i guess that's what it is
-export const tops = [hoodie, dress, tee, undefined];
+export const tops = [
+  dragonHoodie,
+  goodBadBabyTee,
+  tribalTank,
+  xboxPP,
+  undefined,
+];
--- src/components/nav-button.js
+++ src/components/nav-button.js
@@ -1,7 +1,10 @@
+import buttonArrow from "../img/button-arrow.png";
+
 const NavButton = ({ handleClick, buttonClassName }) => {
   return (
     <button className={buttonClassName} onClick={handleClick}>
-      <div className="triangle" />
+      {/* <div className="triangle" /> */}
+      <img src={buttonArrow} className="button-image" />
     </button>
   );
 };
